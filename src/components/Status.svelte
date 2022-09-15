d94fc6df5bf11ef4ed0d4388d0a4ad9a690c725e
KipK <hotlobster75@gmail.com>
2022-09-15 18:11:18 +0200

    wip

--- src/components/Status.svelte
+++ src/components/Status.svelte
@@ -1,33 +1,41 @@
 <div class="box has-background-light">
 	<div class="is-flex-direction-row">
-			<div class="is-narrow is-size-6 has-text-weight-semibold notification">Status&nbsp;
+			<div class="px-0 pt-0 pb-2 is-narrow is-size-6 has-text-weight-semibold notification">Status&nbsp;
 				<span class="tag is-danger">Disabled</span>
 				<div class="p-0 m-0 is-size-6 has-text-weight-semibold has-text-danger notification is-align-content-flex-end">Waiting - EV Not connected</div>
 			</div>
 	</div>
 	<div>
-		<table class="table is-fullwidth mb-0">
-			<thead class="has-background-grey">
-				<tr>
-					<th class="has-text-centered has-text-light py-0 px-0">Current</th>
-					<th class="has-text-centered has-text-light py-0 px-0">Energy</th>
-					<th class="has-text-centered has-text-light py-0 px-0">Temp°</th>
-					<th class="has-text-centered has-text-light py-0 px-0">Elapsed</th>
-				</tr>
-			</thead>
-			<tbody class="has-background-light">
-				<tr>
-					<td class="is-size-5 has-text-centered has-text-weight-semibold">31.9A</td>
-					<td class="is-size-5 has-text-centered has-text-weight-semibold">28.9kWh</td>
-					<td class="is-size-6 has-text-centered has-text-weight-semibold">27.3°C</td>
-					<td class="is-size-6 has-text-centered has-text-weight-semibold">08:27:34</td>
-				</tr>
-			</tbody>
-		</table>
+		<div class="level is-mobile">
+			<div class="box level-item has-text-centered p-2">
+				<div>
+				  <p class="heading has-text-weight-semibold">Current</p>
+				  <p class="is-size-5 has-text-weight-semibold">31.9A</p>
+				</div>
+			  </div>
+			  <div class="box level-item has-text-centered p-2">
+				<div>
+				  <p class="heading has-text-weight-semibold">Energy</p>
+				  <p class="is-size-5 has-text-weight-semibold">123kWh</p>
+				</div>
+			  </div>
+			  <div class="box level-item has-text-centered p-2">
+				<div>
+				  <p class="heading has-text-weight-semibold">Temp°</p>
+				  <p class="is-size-5 has-text-weight-semibold">27.3°C</p>
+				</div>
+			  </div>
+			  <div class="box level-item has-text-centered p-2">
+				<div>
+				  <p class="heading has-text-weight-semibold">Elapsed</p>
+				  <p class="is-size-5 has-text-weight-semibold">08:27:34</p>
+				</div>
+			  </div>
+			</div>
 		<div class="container">
 			<div class="columns is-flex-direction-row is-size-6 pt-2">
-				<div class="px-2"><span class="has-text-weight-bold is-align-content-flex-start">Current Event: </span> <span class="has-text-danger">disabled, 06:25:00</span></div>
-				<div class="px-2"><span class="has-text-weight-bold is-align-content-flex-end">Next Event: </span> <span class="has-text-primary">active, 22:35:00</span></div>
+				<div class="px-2"><span class="has-text-weight-bold is-size-7 is-align-content-flex-start">Current Event: </span> <span class="tag is-white has-text-danger">Disabled 06:25:00</span></div>
+				<div class="px-2"><span class="has-text-weight-bold is-size-7 is-align-content-flex-end">Next Event: </span> <span class="tag is-white has-text-primary">Active 22:35:00</span></div>
 			</div>
 		</div>
 	</div>
