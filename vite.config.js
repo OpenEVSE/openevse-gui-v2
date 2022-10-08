62cac2924c6c3b41f7e74928ff1e990100967444
KipK <hotlobster75@gmail.com>
2022-10-08 10:53:40 +0200

    fix gzip on github page

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(),viteCompression({deleteOriginFile: true, algorithm: "gzip"})],
+  plugins: [svelte(),viteCompression({deleteOriginFile: false, algorithm: "gzip"})],
   css: {
     preprocessorOptions: {
       scss: {
