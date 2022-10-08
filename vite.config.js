940541416504957773fbf1db1881acaad833889a
KipK <hotlobster75@gmail.com>
2022-10-08 10:46:25 +0200

    use gzip compression

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(),viteCompression({deleteOriginFile: true, algorithm: "brotliCompress"})],
+  plugins: [svelte(),viteCompression({deleteOriginFile: true, algorithm: "gzip"})],
   css: {
     preprocessorOptions: {
       scss: {
