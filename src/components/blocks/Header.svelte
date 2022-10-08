c72620f21c3493201e3568d6823f75735342eac6
KipK <hotlobster75@gmail.com>
2022-10-08 09:48:22 +0200

    nav wip

--- src/components/blocks/Header.svelte
+++ src/components/blocks/Header.svelte
@@ -1,6 +1,6 @@
 <script>
   import logo from '../../assets/logo.png';
-  import { link } from "svelte-spa-router";
+  import { link, location } from "svelte-spa-router";
 
   let isBurgerActive = false;
   function toggleBurger() {
@@ -11,10 +11,14 @@
 .burger { margin-left: auto; 
 		  margin-right: 0;
 		}
+.noevent {
+	pointer-events: none;
+	cursor: not-allowed;
+}
 
 </style>
 <div class="container">
-	<nav class="navbar is-white" aria-label="main navigation">
+	<nav class="navbar is-white is-spaced" aria-label="main navigation">
 		<div class="navbar-brand">
 			<a href="/">
 				<img src={logo} class="" alt="logo"/>
@@ -31,16 +35,16 @@
 	
 			<div class="navbar-end is-family-monospace">
 
-				<a class="navbar-item is-size-6 has-text-weight-semibold is-hoverable has-text-grey-dark" href="/" use:link>Main</a>
+				<a class="navbar-item is-size-6 has-text-weight-semibold is-hoverable has-text-grey-dark {$location=="/"?"noevent":""}" class:is-active={$location=="/"} href="/" use:link>Main</a>
 				<div class="navbar-item has-dropdown is-hoverable">
-					<a class="navbar-item is-size-6 has-text-weight-semibold has-text-grey-dark" href="/Config" use:link>Configuration</a>
-					<div class="navbar-dropdown">
-						<a class="navbar-item is-size-6 has-text-weight-semibold is-hoverable has-text-grey-dark" href="/Hardware" use:link>Hardware</a>
-						<a class="navbar-item is-size-6 has-text-weight-semibold is-hoverable has-text-grey-dark" href="/Settings" use:link>Settings</a>
-						<a class="navbar-item is-size-6 has-text-weight-semibold is-hoverable has-text-grey-dark" href="/Charge" use:link>Charge</a>
-						<a class="navbar-item is-size-6 has-text-weight-semibold is-hoverable has-text-grey-dark" href="/MQTT" use:link>MQTT</a>
-						<a class="navbar-item is-size-6 has-text-weight-semibold is-hoverable has-text-grey-dark" href="/Services" use:link>Services</a>
-						<a class="navbar-item is-size-6 has-text-weight-semibold is-hoverable has-text-grey-dark" href="/Vehicle" use:link>Vehicle</a>
+					<a class="navbar-item is-size-6 has-text-weight-semibold has-text-grey-dark navbar-link" href="/Config" class:is-active={$location!="/"} use:link>Configuration</a>
+					<div class="navbar-dropdown is-boxed">
+						<a class="navbar-item is-size-6 has-text-weight-semibold is-hoverable {$location=="/Hardware"?"has-text-grey noevent":"has-text-grey-dark"}" class:is-active={$location=="/Hardware"} href="/Hardware" use:link>Hardware</a>
+						<a class="navbar-item is-size-6 has-text-weight-semibold is-hoverable {$location=="/Settings"?"has-text-grey noevent":"has-text-grey-dark"}" href="/Settings" use:link>Settings</a>
+						<a class="navbar-item is-size-6 has-text-weight-semibold is-hoverable {$location=="/Charge"?"has-text-grey noevent":"has-text-grey-dark"}" href="/Charge" use:link>Charge</a>
+						<a class="navbar-item is-size-6 has-text-weight-semibold is-hoverable {$location=="/MQTT"?"has-text-grey noevent":"has-text-grey-dark"}" href="/MQTT" use:link>MQTT</a>
+						<a class="navbar-item is-size-6 has-text-weight-semibold is-hoverable {$location=="/Services"?"has-text-grey noevent":"has-text-grey-dark"}" href="/Services" use:link>Services</a>
+						<a class="navbar-item is-size-6 has-text-weight-semibold is-hoverable {$location=="/Vehicle"?"has-text-grey noevent":"has-text-grey-dark"}" href="/Vehicle" use:link>Vehicle</a>
 					</div>
 				</div>
 				
