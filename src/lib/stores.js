5899a807e26586f5c7ec22bf1ac8d9eb0e47349f
KipK <hotlobster75@gmail.com>
2022-10-08 23:01:44 +0200

    fix edit

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,10 +1,11 @@
-import { writable, derived } from 'svelte/store';
+import { writable } from 'svelte/store';
 
 /** Store for your data. 
 This assumes the data you're pulling back will be an array.
 If it's going to be an object, default this to an empty object.
 **/
 export const timers_schedule = writable([]);
+fetchSchedule();
 
 /** Data transformation.
 For our use case, we only care about the drink names, not the other information.
@@ -21,3 +22,19 @@ export async function fetchSchedule() {
 		return [];
 	});
 };
+
+export async function deleteSchedule(id) {
+	fetch("/schedule/" + id,{
+	headers: {
+		'Content-Type': 'application/json'
+	  },
+	  method: "delete",
+	})
+	.then(response => response.json())
+	.then(data => {
+		console.log(data);
+	}).catch(error => {
+		console.log(error);
+		return [];
+	});
+}
