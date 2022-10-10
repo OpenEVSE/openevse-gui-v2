9fe9a546b4c87f805546a3686118aaaab330d023
KipK <hotlobster75@gmail.com>
2022-10-10 13:00:36 +0200

    removed loading schedule at each component mount added wip pageloader at page load

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,15 @@
 	import Header from './components/blocks/Header.svelte';
 	import Router from 'svelte-spa-router';
 	import { routes } from "./lib/routes.js";
+	import DataPreload from './components/data/DataPreload.svelte'
 </script>
 
 
 
 <main>
+	
 	<Header />
+	<DataPreload />
 	<Router {routes}/>
 </main>
 
