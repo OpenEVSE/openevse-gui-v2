7e1975b96b917fc8d9af4b19efd64609222f9eaf
KipK <hotlobster75@gmail.com>
2022-10-12 16:47:25 +0200

    simu wip

--- src/lib/stores-simu/plan.js
+++ src/lib/stores-simu/plan.js
@@ -8,7 +8,7 @@ function createPlanStore() {
 	async function download() {
 		P.update(() => plan_fakedata)
 		console.log(P)
-		await new Promise(resolve => setTimeout(resolve, 2000));
+		await new Promise(resolve => setTimeout(resolve, 1000));
 	}
     
 
