86f1f3e2d19aa7f98a5ec87d64e7f6d56e70e080
KipK <hotlobster75@gmail.com>
2022-10-12 14:12:53 +0200

    ...simu

--- src/lib/stores-simu/status.js
+++ src/lib/stores-simu/status.js
@@ -6,9 +6,7 @@ function createStatusStore() {
     const { subscribe, set, update } = P
 
 	async function download() {
-		const response = await new Promise(() => setTimeout( () => {
-		}, 2000))
-			return response
+		await new Promise(resolve => setTimeout(resolve, 2000));
 	}
     
 
