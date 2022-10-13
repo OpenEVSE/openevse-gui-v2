ebd43a72f06f4192fa522103dcdf4c64575de2eb
KipK <hotlobster75@gmail.com>
2022-10-13 21:19:50 +0200

    fix css

--- src/components/ui/ManualButton.svelte
+++ src/components/ui/ManualButton.svelte
@@ -10,5 +10,5 @@
 </style>
 
 <button class="button manual-charge is-size-6 mt-3  {mode == 0?"":"is-outlined"} is-info" on:click={() => { mode = 0}}>Auto</button>
-<button class="button manual-charge is-size-6 mt-3  {mode == 1?"":"is-outlined"} is-outlined is-primary" on:click={() => { mode = 1}}>On</button>
-<button class="button manual-charge is-size-6 mt-3  {mode == 2?"":"is-outlined"} is-outlined is-danger" on:click={() => { mode = 2}}>Off</button>
+<button class="button manual-charge is-size-6 mt-3  {mode == 1?"":"is-outlined"} is-primary" on:click={() => { mode = 1}}>On</button>
+<button class="button manual-charge is-size-6 mt-3  {mode == 2?"":"is-outlined"} is-danger" on:click={() => { mode = 2}}>Off</button>
