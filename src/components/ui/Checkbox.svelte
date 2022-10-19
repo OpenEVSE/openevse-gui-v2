916e6a7d9abdd02752356808c5f660045465f442
KipK <hotlobster75@gmail.com>
2022-10-19 22:44:15 +0200

    UI accessibility

---
+++ src/components/ui/Checkbox.svelte
@@ -0,0 +1,17 @@
+<script>
+export let checked = false
+let focus = false;
+function togglefocus() {
+		focus = !focus
+	}
+</script>
+<style>
+</style>
+<div class="mt-2">
+	<label class="checkradio has-text-weight-semibold focus-no-outline">
+		<input type="checkbox" class="has-tooltip-arrow has-tooltip-right {focus?'is-info':''}" 
+		data-tooltip="Release manual override when vehicle is disconnected" bind:checked={checked}
+		on:focus={togglefocus} on:blur={togglefocus} >
+		Auto-release
+	</label>
+</div>
---
+++ src/components/ui/ExpandArrow.svelte
@@ -0,0 +1,24 @@
+<script>
+import Fa from 'svelte-fa/src/fa.svelte'
+import { faAngleDown, faAngleUp, faAngleRight } from '@fortawesome/free-solid-svg-icons/index.js'
+export let expand
+let size = "sm"
+</script>
+<style>
+.arrow {
+	position: absolute;
+	bottom: 5px;
+	right: 5px;
+}
+</style>
+<div class="arrow">
+	{#if !expand}
+		<button class="button is-dark has-background-light is-inverted" tabindex="0" on:click={() => {expand=true; size="sm"}} on:mouseenter={() => size = "lg"} on:focus={() => size = "lg"} on:mouseleave={() => size = "sm"} on:blur={() => size = "sm"}>
+			<Fa size={size} icon={faAngleDown} />
+		</button>
+	{:else}
+		<button class="button is-dark has-background-light is-inverted" tabindex="0" on:click={() => {expand=false; size="sm"}}  on:mouseenter={() => size = "lg"} on:focus={() => size = "lg"} on:mouseleave={() => size = "sm"} on:blur={() => size = "sm"}>
+			<Fa size={size} icon={faAngleUp} />
+		</button>
+	{/if}
+</div>
