f28f277a20782ad933b58a3f77596bf06278bcf1
KipK <hotlobster75@gmail.com>
2022-11-10 12:40:42 +0100

    polish Auth

--- src/components/blocks/configuration/Auth.svelte
+++ src/components/blocks/configuration/Auth.svelte
@@ -9,18 +9,21 @@
 	let checked = false
 	let submitState = "default"
 	let alert_visible = false
+	let usr
+	let pwd
 
 	let submit = async () => {
 		console.log("save user/pass")
 		submitState = "loading"
-		if (!$config_store.www_username || !$config_store.www_password || $config_store.www_password=='_DUMMY_PASSWORD' ) {
+		if ( checked && (!usr || !pwd || pwd=='_DUMMY_PASSWORD')) {
 			alert_visible = true
+			submitState = "default"
 			return false
 		}
 		else {
 			const data = {
-			www_username: $config_store.www_username,
-			www_password: $config_store.www_password
+			www_username: checked?usr:"",
+			www_password: checked?pwd:""
 			}
 			if (await config_store.upload(data)) 
 				{
@@ -33,19 +36,30 @@
 			}
 		}
 	}
+
+	let getAuthData = (u,p) => {
+		console.log("getAuthData user: " + u + "pwd: " + p)
+		usr = u
+		pwd = p
 		
-	// $: $config_store.www_username, () => {
-	// 		if ($config_store.www_username) 
-	// 			enabled = true
-	// 		else
-	// 			enabled = false
-	// 	}
+		if (u || p)  {
+			checked = true
+			return true
+		}
+			
+		else {
+			checked = null
+			return false
+		}	
+	}
+		
+	$: getAuthData($config_store.www_username,$config_store.www_password)
 
 </script>
 <Box title="Authentification">
-	<Switch name="auth_enabled" label="Enable" tooltip="Enable authenfication for Web Interface & HTTP API " tooltip_pos="right" {checked} />
-	<InputForm title="Username" bind:value={$config_store.www_username} placeholder="Admin" type="text"  />
-	<InputForm title="Password" bind:value={$config_store.www_password} placeholder="15 characters max" type="password" maxlength=15  />
+	<Switch name="auth_enabled" label="Enable" tooltip="Enable authenfication for Web Interface & HTTP API " tooltip_pos="right" bind:checked />
+	<InputForm title="Username" bind:value={usr} placeholder="Admin" type="text" disabled={!checked} />
+	<InputForm title="Password" bind:value={pwd} placeholder="15 characters max" type="password" maxlength=15 disabled={!checked} />
 	<Button name="Save" color="is-info" state={submitState} butn_submit={submit} />
 	
 </Box>
