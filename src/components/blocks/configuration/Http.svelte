b29ea75e1e9a331bf565a09523f13c1bd32a8d51
KipK <hotlobster75@gmail.com>
2022-11-10 17:26:07 +0100

    has_https

--- src/components/blocks/configuration/Http.svelte
+++ src/components/blocks/configuration/Http.svelte
@@ -3,16 +3,19 @@
 	import AlertBox from "./../../ui/AlertBox.svelte";
 	import Switch from "../../ui/Switch.svelte";
 	import { config_store } from "../../../lib/stores/config.js";
+	import {onMount} from "svelte"
 	import InputForm from "../../ui/InputForm.svelte";
 	import Box from "../../ui/Box.svelte"
 	import Button from "../../ui/Button.svelte"
 
-	let checked = false
+	let auth_checked = false
+	let https_checked = false
 	let submitState = "default"
 	let alert_visible = false
 	let usr
 	let pwd
 	let activetab = 0
+	let has_https
 
 	let clickTab = (i) => {
 		activetab = i
@@ -25,15 +28,15 @@
 	
 	let submit = async () => {
 		submitState = "loading"
-		if ( checked && (!usr || !pwd || pwd=='_DUMMY_PASSWORD')) {
+		if ( auth_checked && (!usr || !pwd || pwd=='_DUMMY_PASSWORD')) {
 			alert_visible = true
 			submitState = "default"
 			return false
 		}
 		else {
 			const data = {
-			www_username: checked?usr:"",
-			www_password: checked?pwd:""
+			www_username: auth_checked?usr:"",
+			www_password: auth_checked?pwd:""
 			}
 			if (await config_store.upload(data)) 
 				{
@@ -52,16 +55,27 @@
 		pwd = p
 
 		if (u || p)  {
-			checked = true
+			auth_checked = true
 			return true
 		}
 			
 		else {
-			checked = null
+			auth_checked = null
 			return false
 		}	
 	}
-		
+	
+
+	onMount(() => {
+		if ($config_store.http_supported_protocols) {
+			$config_store.http_supported_protocols.forEach(p => { 
+				if (p === "https") has_https = true
+				else has_https = false
+			})
+		}
+	})
+
+
 	$: getAuthData($config_store.www_username,$config_store.www_password)
 
 </script>
@@ -70,14 +84,20 @@
 	{#if activetab == 0}
 	<!-- Authentification -->
 	<div class="my-2">
-		<Switch name="auth_enabled" label="Enable" tooltip="Enable HTTP authentication with login/password" tooltip_pos="right" bind:checked />
-		<InputForm  title="Username" bind:value={usr} placeholder="Admin" type="text" disabled={!checked} />
-		<InputForm title="Password" bind:value={pwd} placeholder="15 characters max" type="password" maxlength=15 disabled={!checked} />
+		<Switch name="auth_enabled" label="Enable" tooltip="Enable HTTP authentication with login/password" tooltip_pos="right" bind:checked={auth_checked} />
+		<InputForm  title="Username" bind:value={usr} placeholder="Admin" type="text" disabled={!auth_checked} />
+		<InputForm title="Password" bind:value={pwd} placeholder="15 characters max" type="password" maxlength=15 disabled={!auth_checked} />
 		<Button name="Save" color="is-info" state={submitState} butn_submit={submit} />
 	</div>
 	{:else if activetab == 1}
 	<!-- Settings -->
-
+	<div class="my-2">
+		<div class="has-text-weight-semibold">HTTPS</div>
+		<Switch name="https_enabled" label="Enable HTTPS" bind:checked={https_checked} disabled={!has_https} />
+		{#if !has_https}
+		<span class="mx-3 is-size-7 is-italic">HTTPS not supported on current firmware</span>
+		{/if}
+	</div>
 	{/if}
 </Box>
 <AlertBox title="Error" body="Please set a username & password first" bind:visible={alert_visible}/>
--- src/components/ui/Switch.svelte
+++ src/components/ui/Switch.svelte
@@ -2,9 +2,10 @@
 	export let name
 	export let checked
 	export let label
-	export let tooltip
+	export let tooltip = null
 	export let tooltip_pos = "top"
 	export let hidden = false
+	export let disabled = false
 
 	let focus = false
 	function hoverin() {
@@ -17,6 +18,6 @@
 </script>
 <div class="is-flex-shrink-0 mx-3 {tooltip?"has-tooltip-arrow has-tooltip-" + tooltip_pos + " has-tooltip-multiline":""} is-inline-block {hidden?"is-hidden":""}" data-tooltip={tooltip} on:mouseenter={hoverin} on:mouseleave={hoverout}>
 	<input id={name} type="checkbox" name={name} class="switch is-info {focus == true?'':'is-outlined'}" 
-	bind:checked={checked} on:focus={hoverin} on:blur={hoverout} />
+	bind:checked={checked} on:focus={hoverin} on:blur={hoverout} {disabled}/>
 	<label for={name} class="has-text-weight-bold">{label}</label>
 </div>
