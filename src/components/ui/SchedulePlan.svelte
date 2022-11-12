6854250b3fa094650959c3cd12bb6d0c17dd2bdd
KipK <hotlobster75@gmail.com>
2022-11-12 13:37:58 +0100

    fix icon pos

--- src/components/ui/SchedulePlan.svelte
+++ src/components/ui/SchedulePlan.svelte
@@ -24,14 +24,14 @@
 		min-width: 15ch;
 	}
 	.tag-title {
-		min-width: 10ch;
+		min-width: 13ch;
 	}
 </style>
 
 <div class="column is-narrow px-1 pt-1 pb-0">
 
-	<div class="tags has-addons">
-		<span class="tag tag-title is-white has-text-weight-semibold is-size-7"><Fa icon={icon} class="has-text-info mx-2" /> {title}</span>
+	<div class="tags has-addons ">
+		<span class="is-justify-content-left tag tag-title is-white  has-text-weight-semibold is-size-7"><Fa icon={icon} class="has-text-info mx-2" /> {title}</span>
 		<span class="tag tag-val is-capitalized has-text-weight-semibold {state=="active"?"is-primary":"is-danger"}">
 			{#if time != ""}
 			{state} {displayTime(time,tz)}
