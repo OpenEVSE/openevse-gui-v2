6debc20b1ca23b04933664abc0bd6bbe9c6902e9
KipK <hotlobster75@gmail.com>
2022-11-22 19:30:10 +0100

    transitions

--- src/components/ui/Box.svelte
+++ src/components/ui/Box.svelte
@@ -1,11 +1,11 @@
 <script>
 	import { scale, fade,slide } from 'svelte/transition';
-	import { elasticIn, elasticOut, elasticInOut, backInOut, backIn, backOut } from 'svelte/easing';
+	import { elasticIn, elasticOut, elasticInOut, backInOut, backIn, backOut, expoIn, expoOut, expoInOut } from 'svelte/easing';
 	import {onMount,onDestroy} from "svelte"
 	export let title = "title"
 </script>
 
-<div class="box is-unselectable pt-3"  in:scale="{{ delay: 400, duration: 400, easing: backInOut }}" out:scale="{{ delay: 0, duration: 400, easing: backInOut}}">	
+<div class="box is-unselectable pt-3"  in:scale="{{ delay: 300, duration: 400, easing: expoInOut }}" out:fade="{{ delay: 0, duration: 400, easing: expoOut}}">	
 	<div class="is-size-5 has-text-weight-bold mb-1">{title}</div>	
 		<slot>
 		</slot>
