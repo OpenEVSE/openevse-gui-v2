b72a9922e07e3bad5594a97b533c90171f73c3b7
KipK <hotlobster75@gmail.com>
2023-01-12 15:10:35 +0100

    some responsiveness for monitoring tab

--- src/components/blocks/monitoring/Safety.svelte
+++ src/components/blocks/monitoring/Safety.svelte
@@ -12,23 +12,23 @@
 </style>
 <table class="table is-fullwidth">
 	<thead>
-		<tr class="has-background-info">
+		<tr class="has-background-info is-size-7-mobile">
 			<th class="has-text-white py-1">{$_("config.safety.errors")}</th>
 			<th class="has-text-white has-text-centered py-1">{$_("config.safety.count")}</th>
 		</tr>
 	</thead>
 	<tbody>
-		<tr>
+		<tr class="is-size-7-mobile">
 			<td>{$_("config.safety.gfci")}</td>
-			<td class="has-text-centered"><span class="tag {$status_store.gfcicount==0?'is-primary':'is-danger'} is-primary ">{$status_store.gfcicount}</span></td>
+			<td class="has-text-centered"><span class="tag is-normal {$status_store.gfcicount==0?'is-primary':'is-danger'} is-primary ">{$status_store.gfcicount}</span></td>
 		</tr>
-		<tr>
+		<tr class="is-size-7-mobile">
 			<td>{$_("config.safety.noground")}</td>
-			<td class="has-text-centered"><span class="tag {$status_store.nogndcount==0?'is-primary':'is-danger'}">{$status_store.nogndcount}</span></td>
+			<td class="has-text-centered"><span class="tag is-normal {$status_store.nogndcount==0?'is-primary':'is-danger'}">{$status_store.nogndcount}</span></td>
 		</tr>
-		<tr>
+		<tr class="is-size-7-mobile">
 			<td>{$_("config.safety.stuck")}</td>
-			<td class="has-text-centered"><span class="tag {$status_store.stuckcount==0?'is-primary':'is-danger'}">{$status_store.stuckcount}</span></td>
+			<td class="has-text-centered"><span class="tag is-normal {$status_store.stuckcount==0?'is-primary':'is-danger'}">{$status_store.stuckcount}</span></td>
 		</tr>
 	</tbody>
 </table>
