cf67e7b2006f354468efd924ff4afb54ed047ec8
KipK <hotlobster75@gmail.com>
2023-01-16 00:15:30 +0100

    layouts

--- src/components/blocks/configuration/WifiDisplay.svelte
+++ src/components/blocks/configuration/WifiDisplay.svelte
@@ -9,12 +9,17 @@
 	.nopointer {
 		cursor: default;
     }
+	.content {
+		max-width: 500px;
+		margin-left: auto;
+		margin-right: auto;;
+	}
 </style>
-<div class="container">
+<div class="content">
 	<table class="table has-text-centered is-fullwidth is-bordered is-size-7">
 		<thead>
 			<tr class="has-background-info">
-			<th class="has-text-white has-text-centered" style="width: 70%;">{$_("config.network.ssid")}</th>
+			<th class="has-text-white has-text-centered" >{$_("config.network.ssid")}</th>
 			<th class="has-text-white has-text-centered">{$_("config.network.signal")}</th>
 			</tr>
 		</thead>
--- src/components/blocks/configuration/WifiScan.svelte
+++ src/components/blocks/configuration/WifiScan.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Borders from "./../../ui/Borders.svelte";
 	import AlertBox from "./../../ui/AlertBox.svelte";
 	import { _ } 		  					 from 'svelte-i18n'
 	import { uistates_store } 				 from "./../../../lib/stores/uistates.js";
@@ -106,60 +107,64 @@
 		table {
 			height:100%;
 		}
+		.content {
+			max-width: 500px;
+			margin-left: auto;
+			margin-right: auto;;
+		}
 </style>
 <div>
-	<table class="table is-hoverable has-text-centered is-fullwidth is-bordered is-size-7 mb-3">
-		<thead>
-			<tr class="has-background-info ">
-				<th class="has-text-white has-text-centered" style="width: 70%;">{$_("config.network.ssid")}</th>
-				<th class="has-text-white has-text-centered">{$_("config.network.signal")}</th>
-			</tr>
-		</thead>
-		<tbody>
-				{#if networks && networks.length > 0}
-					{#each networks as network}
-						<tr class="has-background-light">
-							<td class="m-0 p-0"><button class=" is-clickable cellbutton has-text-weight-semibold" on:click={()=> {ssid=network.ssid}}>{network.ssid}</button></td>
-							<td class="pt-2 no-pointer has-tooltip-arrow has-tooltip-top nopointer" data-tooltip={network.rssi + " dBm"}>
-								<WifiIcon dbm={network.rssi}/>
-							</td>
-						</tr>
-					{/each}
-				{:else if state == "scan"}
-				<tr class="has-background-light">
-					<td class="has-text-centered is-vcentered has-text-weight-semibold">{$_("config.network.scanning")}</td>
-					<td class="has-text-info is-size-5"><Loader /></td>
+	<div class="content">
+		<table class="table is-hoverable has-text-centered is-fullwidth is-bordered is-size-7 mb-3">
+			<thead>
+				<tr class="has-background-info ">
+					<th class="has-text-white has-text-centered">{$_("config.network.ssid")}</th>
+					<th class="has-text-white has-text-centered">{$_("config.network.signal")}</th>
 				</tr>
-				{:else}
+			</thead>
+			<tbody>
+					{#if networks && networks.length > 0}
+						{#each networks as network}
+							<tr class="has-background-light">
+								<td class="m-0 p-0"><button class=" is-clickable cellbutton has-text-weight-semibold" on:click={()=> {ssid=network.ssid}}>{network.ssid}</button></td>
+								<td class="pt-2 no-pointer has-tooltip-arrow has-tooltip-top nopointer" data-tooltip={network.rssi + " dBm"}>
+									<WifiIcon dbm={network.rssi}/>
+								</td>
+							</tr>
+						{/each}
+					{:else if state == "scan"}
 					<tr class="has-background-light">
-						<th class="has-text-centered is-vcentered has-text-weight-semibold">{$_("config.network.failed")}</th>
-						<td class="has-text-info is-size-6"></td>
+						<td class="has-text-centered is-vcentered has-text-weight-semibold">{$_("config.network.scanning")}</td>
+						<td class="has-text-info is-size-5"><Loader /></td>
 					</tr>
-				{/if}
-		</tbody>
+					{:else}
+						<tr class="has-background-light">
+							<th class="has-text-centered is-vcentered has-text-weight-semibold">{$_("config.network.failed")}</th>
+							<td class="has-text-info is-size-6"></td>
+						</tr>
+					{/if}
+			</tbody>
 
-	</table>
+		</table>
+	</div>
 
 
 	<form on:submit|preventDefault>
-		<div class="is-flex is-flex-direction-column is-justify-content-center">
-			<div class="container">
-				<InputForm type="text" title={$_("config.network.ssid")} placeholder={$_("config.network.ssid-desc")} bind:value={ssid} />
-				<InputForm type="password" title={$_("config.network.pass")} placeholder={$_("config.network.pass-desc")} bind:value={key} />
-			</div>
-			
-			
+		<div class="is-flex is-justify-content-center ">
+			<Borders>
+				<div class="is-flex is-flex-direction-column is-justify-content-center">
+					<InputForm type="text" title={$_("config.network.ssid")} placeholder={$_("config.network.ssid-desc")} bind:value={ssid} />
+					<InputForm type="password" title={$_("config.network.pass")} placeholder={$_("config.network.pass-desc")} bind:value={key} />
+				</div>
+				<div class="is-flex is-align-items-center is-justify-content-center">
+					<Button name={$_("config.network.connect")} color="is-primary" bind:state={connectButnState} butn_submit={connectWifi} disabled={ssid =="" || key == ""?true:false}/>
+					<Button name={$_("config.network.scan")} butn_submit={scanAgain} bind:state={scanButnState}/>
+					{#if $config_store.ssid}
+					<Button name={$_("cancel")} color="is-danger" butn_submit={() => active = false}/>
+					{/if}
+				</div>
+			</Borders>
 		</div>
-			
-			<div class="is-flex is-align-items-center is-justify-content-center">
-				<Button name={$_("config.network.connect")} color="is-primary" bind:state={connectButnState} butn_submit={connectWifi} disabled={ssid =="" || key == ""?true:false}/>
-				<Button name={$_("config.network.scan")} butn_submit={scanAgain} bind:state={scanButnState}/>
-				{#if $config_store.ssid}
-				<Button name={$_("cancel")} color="is-danger" butn_submit={() => active = false}/>
-				{/if}
-			</div>
 	</form>
-
-	
 </div>
 <AlertBox title={$_("notification")} body={$_("config.network.redirect")} bind:visible={alertbox_redirect}/>
