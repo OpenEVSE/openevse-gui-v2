bbb08fcfad6920860427cf040f3032cf63e71282
KipK <hotlobster75@gmail.com>
2023-01-17 19:10:32 +0100

    relmove unnecessary files and css components

--- src/main-portal.js
+++
@@ -1,9 +0,0 @@
-import "./app.scss";
-import "./mystyles.scss"
-import App from "./WifiPortal.svelte";
-
-const app = new App({
-  target: document.getElementById("app"),
-});
-
-export default app;
