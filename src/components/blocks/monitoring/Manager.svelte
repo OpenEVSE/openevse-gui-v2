317b1f321b3257cc6a40968275877a85e90451d9
KipK <hotlobster75@gmail.com>
2023-01-18 11:08:55 +0100

    Manager tab tags size

--- src/components/blocks/monitoring/Manager.svelte
+++ src/components/blocks/monitoring/Manager.svelte
@@ -5,12 +5,21 @@
 	import {config_store} 		   from "../../../lib/stores/config.js"
 	import {clientid2name, displayIcon} 		   from "../../../lib/utils.js"
 </script>
-
+<style>
+	.tag.service {
+		box-sizing: border-box;
+		width: 14ch;
+	}
+	.tag.val {
+		box-sizing: border-box;
+		width: 12ch;
+	}
+</style>
 <table class="table is-fullwidth">
 	<thead class="is-size-7-mobile">
 		<tr class="has-background-info"	>
 			<th class="has-text-white has-text-left py-1">{$_("monitoring-manager-prop")}</th>
-			<th class="has-text-white has-text-centered py-1">{$_("monitoring-manager-service")} | {$_("monitoring-manager-val")}</th>
+			<th class="has-text-white has-text-centered py-1" style="width:40%">{$_("monitoring-manager-service")} | {$_("monitoring-manager-val")}</th>
 		</tr>
 	</thead>
 	<tbody>
@@ -19,11 +28,11 @@
 			<td class="has-text-left is-italic is-size-7-mobile">state</td>
 			<td class="has-text-centered is-capitalized is-flex is-justify-content-center">
 				<div class="tags has-addons">
-					<span class="tag is-size-7-mobile is-info has-text-weight-bold">
+					<span class="tag service is-size-7-mobile is-info has-text-weight-bold">
 						<iconify-icon  class="has-text-white mr-1" icon={displayIcon(clientid2name($claims_target_store.claims.state))}></iconify-icon>
 						{clientid2name($claims_target_store.claims.state)}
 					</span>
-					<span class="tag has-text-weight-bold is-size-7-mobile {$claims_target_store.properties.state == "active"?"is-primary":"is-danger"} is-capitalized">
+					<span class="tag val has-text-weight-bold is-size-7-mobile {$claims_target_store.properties.state == "active"?"is-primary":"is-danger"} is-capitalized">
 						{$_($claims_target_store.properties.state)}
 					</span>
 				</div>
@@ -35,11 +44,11 @@
 			<td class="has-text-left is-italic is-size-7-mobile">max_current</td>
 			<td class="has-text-centered is-capitalized is-flex is-justify-content-center">
 				<div class="tags has-addons">
-					<span class="tag is-info has-text-weight-bold is-size-7-mobile">
+					<span class="tag service is-info has-text-weight-bold is-size-7-mobile">
 						<iconify-icon  class="has-text-white mr-1" icon={displayIcon(clientid2name($claims_target_store.claims.max_current))}></iconify-icon>
 						{clientid2name($claims_target_store.claims.max_current)}
 					</span>
-					<span class="tag has-text-weight-bold is-info is-capitalized is-size-7-mobile">
+					<span class="tag val has-text-weight-bold is-info is-capitalized is-size-7-mobile">
 						{$claims_target_store.properties.max_current}
 					</span>
 				</div>
@@ -51,11 +60,11 @@
 			<td class="has-text-left is-italic is-size-7-mobile">charge_current</td>
 			<td class="has-text-centered is-capitalized is-flex is-justify-content-center">
 				<div class="tags has-addons">
-					<span class="tag is-info has-text-weight-bold is-size-7-mobile">
+					<span class="tag service is-info has-text-weight-bold is-size-7-mobile">
 						<iconify-icon  class="has-text-white mr-1" icon={displayIcon(clientid2name($claims_target_store.claims.charge_current))}></iconify-icon>
 						{clientid2name($claims_target_store.claims.charge_current)}
 					</span>
-					<span class="tag has-text-weight-bold is-info is-capitalized is-size-7-mobile">
+					<span class="tag val has-text-weight-bold is-info is-capitalized is-size-7-mobile">
 						{$claims_target_store.properties.charge_current}
 					</span>
 				</div>
@@ -67,11 +76,11 @@
 			<td class="has-text-left is-italic is-size-7-mobile">auto_release</td>
 			<td class="has-text-centered is-capitalized is-flex is-justify-content-center">
 				<div class="tags has-addons">
-					<span class="tag is-info has-text-weight-bold is-size-7-mobile">
+					<span class="tag service is-info has-text-weight-bold is-size-7-mobile">
 						<iconify-icon  class="has-text-white mr-1" icon={displayIcon(clientid2name($claims_target_store.claims.auto_release))}></iconify-icon>
 						{clientid2name($claims_target_store.claims.auto_release)}
 					</span>
-					<span class="tag has-text-weight-bold is-info is-capitalized is-size-7-mobile">
+					<span class="tag val has-text-weight-bold is-info is-capitalized is-size-7-mobile">
 						{$claims_target_store.properties.auto_release}
 					</span>
 				</div>
@@ -83,11 +92,11 @@
 			<td class="has-text-left is-italic is-size-7-mobile">time_limit</td>
 			<td class="has-text-centered is-capitalized is-flex is-justify-content-center">
 				<div class="tags has-addons">
-					<span class="tag has-text-weight-bold is-info has-text-weight-bold is-size-7-mobile">
+					<span class="tag service has-text-weight-bold is-info has-text-weight-bold is-size-7-mobile">
 						<iconify-icon  class="has-text-white mr-1" icon={displayIcon(clientid2name($claims_target_store.claims.time_limit))}></iconify-icon>
 						{clientid2name($claims_target_store.claims.time_limit)}
 					</span>
-					<span class="tag is-info is-capitalized is-size-7-mobile">
+					<span class="tag val is-info is-capitalized is-size-7-mobile">
 						{$claims_target_store.properties.time_limit}
 					</span>
 				</div>
@@ -99,11 +108,11 @@
 			<td class="has-text-left is-italic is-size-7-mobile	">energy_limit</td>
 			<td class="has-text-centered is-capitalized is-flex is-justify-content-center">
 				<div class="tags has-addons">
-					<span class="tag is-info has-text-weight-bold is-size-7-mobile">
+					<span class="tag service is-info has-text-weight-bold is-size-7-mobile">
 						<iconify-icon  class="has-text-white mr-1" icon={displayIcon(clientid2name($claims_target_store.claims.energy_limit))}></iconify-icon>
 						{clientid2name($claims_target_store.claims.energy_limit)}
 					</span>
-					<span class="tag has-text-weight-bold is-info is-capitalized is-size-7-mobile">
+					<span class="tag val has-text-weight-bold is-info is-capitalized is-size-7-mobile">
 						{$claims_target_store.properties.energy_limit}
 					</span>
 				</div>
