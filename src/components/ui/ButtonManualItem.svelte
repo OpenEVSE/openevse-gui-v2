76d04748eb8dea26624950c328eb4d9125b9f606
KipK <hotlobster75@gmail.com>
2023-01-19 16:25:33 +0100

    refactored manual buttons

---
+++ src/components/ui/ButtonManualItem.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { onMount } from "svelte";
+	import { _ } from 'svelte-i18n'
+
+	export let mode		 // 0: Auto, 1: On, 2: OFF
+	export let role      // 0: Auto, 1: On, 2: OFF
+	export let ischarging = false
+	export let onClick = (m) => {}
+	export let disabled = false
+	export let icon
+	export let tooltip
+	let color
+
+	onMount(()=>{
+		color = role == 0 ? "is-info" : role == 1 ? "is-primary" : "is-danger"
+	})
+</script>
+<style>
+	.button  {
+		margin: 0;
+		min-height: 50px;
+	}
+	.no-pointer {
+		cursor: default;
+		pointer-events: hover;
+	}
+</style>
+
+
+
+<button {disabled} class="{mode==role || disabled?"no-pointer":""} {mode == role?"":"is-outlined"} 
+is-large is-responsive button has-tooltip-arrow {color} {mode == role && ischarging?"has-text-warning":""}"
+data-tooltip={disabled?null:tooltip}
+on:click|preventDefault={onClick}>
+<div class="mx-4  is-flex">
+	<iconify-icon class="is-flex-grow-1 is-flex-shrink-0 is-size-3" icon={icon}></iconify-icon>
+</div>
+
+</button>
