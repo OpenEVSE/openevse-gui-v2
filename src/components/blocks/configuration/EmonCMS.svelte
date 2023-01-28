0c934c206c7b3ca74d5ab21b7ebfbd3a9e192fa6
KipK <hotlobster75@gmail.com>
2023-01-28 16:01:17 +0100

    emoncms + ohmconnect form

--- src/components/blocks/configuration/EmonCMS.svelte
+++ src/components/blocks/configuration/EmonCMS.svelte
@@ -1,85 +1,123 @@
 <script>
-	import { _ } 		    from 'svelte-i18n'
-	import InputForm 		from "./../../ui/InputForm.svelte";
-	import Box 				from "../../ui/Box.svelte";
-	import Button 			from "./../../ui/Button.svelte";
-	import Switch			from "./../../ui/Switch.svelte";
-	import AlertBox 		from "../../ui/AlertBox.svelte"
-	import { status_store } from "./../../../lib/stores/status.js";
-	import { serialQueue }  from "./../../../lib/queue.js";
-	import { config_store } from "./../../../lib/stores/config.js";
-	
-	let stg_submit_state
-	let alert_body
-	let alert_visible = false
+	import { _ } 		  	 	from 'svelte-i18n'
+	import { onMount } 			from "svelte";
+	import { status_store } 	from "../../../lib/stores/status.js";
+	import { config_store } 	from "../../../lib/stores/config.js";
+	import { submitFormData } 	from "../../../lib/utils.js"
+	import InputForm 			from "../../ui/InputForm.svelte";
+	import Box 					from "../../ui/Box.svelte";
+	import Switch				from "../../ui/Switch.svelte";
+	import Borders 				from "../../ui/Borders.svelte";
 
-	async function toggleEmonCMS() {	
-		let res = await serialQueue.add(() => config_store.saveParam("emoncms_enabled", $config_store.emoncms_enabled))
+	let mounted = false
+	const hiddenpass = "••••••••••"
+	let formdata = {
+			emoncms_enabled: {val: false, status: "", input: undefined, req: false},
+			emoncms_server:	{val: null,  status: "", input: undefined, req: true},
+			emoncms_node:	{val: null,  status: "", input: undefined, req: true},
+			emoncms_apikey:	{val: null,  status: "", input: undefined, req: true, pwd: true}
+		}	
+
+	let updateFormData = () => {
+		formdata.emoncms_enabled.val = $config_store.emoncms_enabled
+		formdata.emoncms_server.val = $config_store.emoncms_server	
+		formdata.emoncms_node.val = $config_store.emoncms_node	
+		formdata.emoncms_apikey.val = $config_store.emoncms_apikey?hiddenpass:""
 	}
 
-	let stg_submit = async () => {
-		if (!$config_store.emoncms_server) {
-			alert_body = $_("config.emon.missing-server")
-			alert_visible=true
-			return
-		}
-		else if (!$config_store.emoncms_node) {
-			alert_body = $_("config.emon.missing-node")
-			alert_visible=true
-			return
-		}
-		else if (!$config_store.emoncms_apikey) {
-			alert_body = $_("config.emon.missing-key")
-			alert_visible=true
-			return
-		}
-
-		stg_submit_state = "loading"
-	
-		const data = {
-			emoncms_server: $config_store.emoncms_server,
-			emoncms_node: $config_store.emoncms_node,
-			emoncms_apikey: $config_store.emoncms_apikey
-		}
-
-		if (await config_store.upload(data)) 
-			{
-				stg_submit_state = "ok"
-				return true
-			}
-		else {
-			stg_submit_state = "error"
-			return false
-		}
-	
+	let toggleEmonCMS = async () => {
+		await submitFormData({form: formdata, prop_enable: "emoncms_enabled", i18n_path: "config.emon.missing-"})
 	}
+
+
+	let setProperty = async (prop) => {
+		await submitFormData({prop: prop, form: formdata , prop_enable: "emoncms_enabled", i18n_path: "config.emon.missing-"})
+	}
+
+	onMount( () => {
+		updateFormData()
+		mounted = true
+
+	})
 </script>
+<style>
+	.enable {
+		min-width: 230px;
+	}
+	
+</style>
 
 <Box title={$_("config.titles.emon")} icon="fa6-solid:chart-bar" back={true}>
-	<div>
-		<Switch name="emoncmsswitch" label={$_("config.emon.enable")} onChange={toggleEmonCMS} bind:checked={$config_store.emoncms_enabled} is_rtl={true}/>
+	<div class="columns is-centered">
+		<div class="column is-three-quarters is-full-mobile">
+			
+			<div class="mb-2 enable is-flex is-align-items-center is-justify-content-center">
+				<Borders classes={$config_store.ohm_enabled?"has-background-primary-light":"has-background-light"}>
+					<Switch 
+					name="emoncmsswitch" 
+					label={$_("enable")} 
+					onChange={toggleEmonCMS} 
+					bind:this={formdata.emoncms_enabled.input} 
+					bind:checked={formdata.emoncms_enabled.val} 
+					bind:status={formdata.emoncms_enabled.status} 
+					disabled={formdata.emoncms_enabled.status == "loading"}
+					/>
+					<!-- <Switch name="emoncmsswitch" label={$_("config.emon.enable")} onChange={toggleEmonCMS} bind:checked={$config_store.emoncms_enabled} is_rtl={true}/> -->
+					{#if $config_store.emoncms_enabled}
+					<div class="is-size-7">
+						<span class="">{$_("config.emon.connected")}:</span>
+						{#key $status_store.emoncms_connected }
+						<span class="has-text-weight-bold {$status_store.emoncms_connected == 1 ?"has-text-primary":"has-text-danger"}">{$status_store.emoncms_connected == 1 ?$_("yes"):$_("no")}</span>
+						{/key}
+						<span class="ml-2">{$_("config.emon.posts")}:</span>
+						<span class="">{$status_store.packets_success}/{$status_store.packets_sent}</span>
+					</div>
+					{/if}
+				</Borders>
+			</div>
+			<div class="mb-2 is-flex is-align-items-center is-justify-content-center">
+				<Borders grow>
+					<InputForm 
+						title="{$_("config.emon.server")}*" 
+						bind:this={formdata.emoncms_server.input} 
+						type="txt" bind:value={formdata.emoncms_server.val} 
+						bind:status={formdata.emoncms_server.status} 
+						disabled={formdata.emoncms_server.status == "loading"} 
+						placeholder="" 
+						onChange={()=>setProperty("emoncms_server")}
+					/>
+					<div class="is-size-7">e.g: <a href="https://data.openevse.com/emoncms">data.openevse.com/emoncms</a>, <a href="https://emoncms.org/">emoncms.org</a>, <a href="http://emonpi/">emonpi</a></div>
+					
+					<InputForm 
+						title="{$_("config.emon.node")}*" 
+						bind:this={formdata.emoncms_node.input} 
+						type="txt" bind:value={formdata.emoncms_node.val} 
+						bind:status={formdata.emoncms_node.status} 
+						disabled={formdata.emoncms_node.status == "loading"} 
+						placeholder="" 
+						onChange={()=>setProperty("emoncms_node")}
+					/>
+					<InputForm 
+						title="{$_("config.emon.apikey")}*" 
+						bind:this={formdata.emoncms_apikey.input} 
+						type="password" bind:value={formdata.emoncms_apikey.val} 
+						bind:status={formdata.emoncms_apikey.status} 
+						disabled={formdata.emoncms_apikey.status == "loading"} 
+						placeholder="" 
+						onChange={()=>setProperty("emoncms_apikey")}
+					/>
+				</Borders>
+			</div>
+		
+		</div>
 	</div>
 
-	{#if $config_store.emoncms_enabled}
-	<div class="is-size-7">
-		<span class="">{$_("config.emon.connected")}:</span>
-		{#key $status_store.emoncms_connected }
-		<span class="has-text-weight-bold {$status_store.emoncms_connected == 1 ?"has-text-primary":"has-text-danger"}">{$status_store.emoncms_connected == 1 ?$_("yes"):$_("no")}</span>
-		{/key}
-		<span class="ml-2">{$_("config.emon.posts")}:</span>
-		<span class="">{$status_store.packets_success}/{$status_store.packets_sent}</span>
-	</div>
-	{/if}
-	
-	<InputForm title="{$_("config.emon.server")}*:" bind:value={$config_store.emoncms_server} placeholder="server" />
-	<div class="is-size-7">e.g: <a href="https://data.openevse.com/emoncms">data.openevse.com/emoncms</a>, <a href="https://emoncms.org/">emoncms.org</a>, <a href="http://emonpi/">emonpi</a></div>
-
-	<InputForm title="{$_("config.emon.node")}*:" bind:value={$config_store.emoncms_node} placeholder="node name" />
-	<InputForm title="{$_("config.emon.apikey")}*:" type="password" bind:value={$config_store.emoncms_apikey} placeholder="api key" />
 
 	
+	
+
+<!-- 	
 	<div class="block mt-5 mb-1">
 		<Button name={$_("save")} color="is-info" state={stg_submit_state} butn_submit={stg_submit} />
-	</div>
-	<AlertBox title={$_("error")} body={alert_body} bind:visible={alert_visible} />
+	</div> -->
 </Box>
--- src/components/blocks/configuration/OhmConnect.svelte
+++ src/components/blocks/configuration/OhmConnect.svelte
@@ -1,4 +1,5 @@
 <script>
+	import OhmHelp from "./../../help/OhmHelp.svelte";
 	import { onMount } 			from "svelte";
 	import { _ } 		    	from 'svelte-i18n'
 	import { status_store } 	from "./../../../lib/stores/status.js";
@@ -11,14 +12,15 @@
 
 
 	let mounted = false
+	const hiddenpass = "••••••••••"
 	let formdata = {
 			ohm_enabled: {val: false, status: "", input: undefined, req: false},
-			ohm:	{val: null,  status: "", input: undefined, req: true}
+			ohm:	{val: null,  status: "", input: undefined, req: true, pwd: true}
 		}	
 
 	let updateFormData = () => {
 		formdata.ohm_enabled.val = $config_store.ohm_enabled
-		formdata.ohm.val = $config_store.ohm	
+		formdata.ohm.val = $config_store.ohm?hiddenpass:""
 	}
 
 	let toggleOhm = async () => {
@@ -37,18 +39,25 @@
 	})
 
 </script>
+<style>
+	.enable {
+		min-width: 230px;
+	}
+	
+</style>
 
 {#if mounted}
-<Box title={$_("config.titles.ohm")} icon="mdi:energy-circle" back={true}>
+<Box title={$_("config.titles.ohm")} icon="mdi:energy-circle" back={true} has_help={true}>
+	<div slot="help"><OhmHelp /></div>
 	<div class="columns is-centered">
 		<div class="column is-three-quarters is-full-mobile">
 			
-			<div class="mb-2 is-flex is-align-items-center is-justify-content-center">
+			<div class="mb-2 enable is-flex is-align-items-center is-justify-content-center">
 				<Borders classes={$config_store.ohm_enabled?"has-background-primary-light":"has-background-light"}>
 					<div>
 						<Switch 
-						name="emoncmsswitch" 
-						label={$_("config.ohm.enable")} 
+						name="ohmswitch" 
+						label={$_("enable")} 
 						onChange={toggleOhm} 
 						bind:this={formdata.ohm_enabled.input} 
 						bind:checked={formdata.ohm_enabled.val} 
@@ -57,30 +66,27 @@
 						/>
 					
 					</div>
+					{#if $config_store.ohm_enabled}
+					<div class="is-size-7">
+						<span class="">Ohm Hour:</span>
+						<span class="has-text-weight-bold {$status_store.ohm_hour == "NotConnected" ?"has-text-danger":"has-text-primary"}">{$status_store.ohm_hour}</span>
+					</div>
+					{/if}
 				</Borders>
 			</div>
-		
-			<div class="is-size-7 mb-2">{$_("config.ohm.desc")}</div>
-			{#if $config_store.ohm_enabled}
-			<div class="is-size-7">
-				<span class="">Ohm Hour:</span>
-				<span class="has-text-weight-bold {$status_store.ohm_hour == "NotConnected" ?"has-text-danger":"has-text-primary"}">{$status_store.ohm_hour}</span>
-			</div>
-			{/if}
-			
-			<InputForm 
-			title="{$_("config.ohm.ohmkey")}*" 
-			bind:this={formdata.ohm.input} 
-			type="txt" bind:value={formdata.ohm.val} 
-			bind:status={formdata.ohm.status} 
-			disabled={formdata.ohm.status == "loading"} 
-			placeholder="" 
-			onChange={()=>setProperty("ohm")}
-		/>
-			<div class="is-size-7">
-				{$_("config.ohm.ohmkey-desc")}<br>
-				<span class="has-text-weight-bold">{$_("config.ohm.example")}: </span><span>https://login.ohmconnect.com/verify-ohm-hour/OpnEoVse</span><br>
-				<span class="has-text-weight-bold">{$_("config.ohm.key")}:</span>OpnEoVse
+
+			<div class="mb-2 is-flex is-align-items-center is-justify-content-center is-flex-direction-column ">
+				<Borders grow>
+					<InputForm
+						title="{$_("config.ohm.ohmkey")}*" 
+						bind:this={formdata.ohm.input} 
+						type="password" bind:value={formdata.ohm.val} 
+						bind:status={formdata.ohm.status} 
+						disabled={formdata.ohm.status == "loading"} 
+						placeholder="" 
+						onChange={()=>setProperty("ohm")}
+					/>
+				</Borders>
 			</div>
 		</div>
 	</div>
