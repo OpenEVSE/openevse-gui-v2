53db3fb7ae59db9d8d6dc28ed04272c839a1a3b4
KipK <hotlobster75@gmail.com>
2023-01-28 16:52:29 +0100

    OCPP form

--- src/components/blocks/configuration/Ocpp.svelte
+++ src/components/blocks/configuration/Ocpp.svelte
@@ -1,120 +1,204 @@
 <script>
-	import AlertBox from "./../../ui/AlertBox.svelte";
-	import { status_store } from "./../../../lib/stores/status.js";
-	import Switch from "./../../ui/Switch.svelte";
-	import { config_store }	 from "./../../../lib/stores/config.js";
-	import { serialQueue }	 from "./../../../lib/queue.js";
-	import OcppHelp 		 from "./../../help/OcppHelp.svelte";
-	import Borders 			 from "./../../ui/Borders.svelte";
-	import { _ } 		   	 from 'svelte-i18n'
-	import InputForm 		 from "./../../ui/InputForm.svelte";
-	import Button 			 from "./../../ui/Button.svelte";
-	import Box 				 from "./../../ui/Box.svelte";
+	import { status_store } 	from "./../../../lib/stores/status.js";
+	import { config_store }	 	from "./../../../lib/stores/config.js";
+	import { serialQueue }	 	from "./../../../lib/queue.js";
+	import { submitFormData } 	from "./../../../lib/utils.js";
+	import { onMount } 			from "svelte";
+	import { _ } 		   	 	from 'svelte-i18n';
+	import OcppHelp 		 	from "./../../help/OcppHelp.svelte";
+	import Borders 			 	from "./../../ui/Borders.svelte";
+	import InputForm 		 	from "./../../ui/InputForm.svelte";
+	import Button 			 	from "./../../ui/Button.svelte";
+	import Box 				 	from "./../../ui/Box.svelte";
+	import Switch			 	from "./../../ui/Switch.svelte";
 
-	let alert_body
-	let alert_visible = false
-	let stg_submit_state
-	let stg_submit = async () => {
+	let mounted = false
+	const hiddenpass = "••••••••••"
+	let formdata = {
+			ocpp_enabled: 		{val: false,  status: "", input: undefined, req: false},
+			ocpp_chargeBoxId:	{val: null,   status: "", input: undefined, req: true},
+			ocpp_server:		{val: null,   status: "", input: undefined, req: true},
+			ocpp_authkey:		{val: null,   status: "", input: undefined, req: true, pwd: true},
+			ocpp_auth_auto:		{val: false,  status: "", input: undefined, req: false},
+			ocpp_idtag:			{val: "",     status: "", input: undefined, req: false},
+			ocpp_auth_offline:	{val: false,  status: "", input: undefined, req: false},
+			ocpp_suspend_evse:	{val: false,  status: "", input: undefined, req: false},
+			ocpp_energize_plug:	{val: false,  status: "", input: undefined, req: false}
+		}	
 
-		if (!$config_store.ocpp_server) {
-			alert_body = $_("config.ocpp.missing-server")
-			alert_visible=true
-			return false
-		}
-		if (!$config_store.ocpp_chargeBoxId) {
-			alert_body = $_("config.ocpp.missing-chargeBoxId")
-			alert_visible=true
-			return false
-		}
-		if (!$config_store.ocpp_authkey) {
-			alert_body = $_("config.ocpp.missing-authkey")
-			alert_visible=true
-			return false
-		}
-		stg_submit_state = "loading"
-
-		const data = {
-			ocpp_server: $config_store.ocpp_server,
-			ocpp_chargeBoxId: $config_store.ocpp_chargeBoxId,
-			ocpp_authkey: $config_store.ocpp_authkey,
-			ocpp_auth_auto: $config_store.ocpp_auth_auto,
-			ocpp_auth_offline: $config_store.ocpp_auth_offline,
-			ocpp_suspend_evse: $config_store.ocpp_suspend_evse,
-			ocpp_energize_plug: $config_store.ocpp_energize_plug
-		}
-
-		if ($config_store.ocpp_auth_auto) {
-			if (!$config_store.ocpp_idtag) {
-				alert_body = $_("config.ocpp.missing-idtag")
-				alert_visible=true
-				return false
-			}
-			data.ocpp_idtag = $config_store.ocpp_idtag
-		}
-			
-
-		if (await config_store.upload(data)) 
-			{
-				stg_submit_state = "ok"
-				return true
-			}
-		else {
-			stg_submit_state = "error"
-			return false
-		}
+	let updateFormData = () => {
+		formdata.ocpp_enabled.val = $config_store.ocpp_enabled
+		formdata.ocpp_chargeBoxId.val = $config_store.ocpp_chargeBoxId
+		formdata.ocpp_server.val = $config_store.ocpp_server
+		formdata.ocpp_authkey.val = $config_store.ocpp_authkey?hiddenpass:""
+		formdata.ocpp_auth_auto.val = $config_store.ocpp_auth_auto
+		if (formdata.ocpp_auth_auto.val)
+			formdata.ocpp_idtag.req = true
+		formdata.ocpp_idtag.val	 = $config_store.ocpp_idtag
+		formdata.ocpp_auth_offline.val = $config_store.ocpp_auth_offline
+		formdata.ocpp_suspend_evse.val = $config_store.ocpp_suspend_evse
+		formdata.ocpp_energize_plug.val = $config_store.ocpp_energize_plug
 	}
 
-	async function toggleOCPP() {	
-		await serialQueue.add(() => config_store.saveParam("ocpp_enabled", $config_store.ocpp_enabled))
+	
+	let toggleOCPP = async () => {
+		await submitFormData({form: formdata, prop_enable: "ocpp_enabled", i18n_path: "config.ocpp.missing-"})
 	}
 
 
+	let setProperty = async (prop) => {
+		if (prop == "ocpp_auth_auto")
+			formdata.ocpp_idtag.req = formdata.ocpp_auth_auto.val	
+		await submitFormData({prop: prop, form: formdata , prop_enable: "ocpp_enabled", i18n_path: "config.ocpp.missing-"})
+	}
 
+	onMount( () => {
+		updateFormData()
+		Object.keys(formdata).forEach(key => {
+			if (formdata[key].val == undefined) {
+				formdata[key].val = ""
+			}
+		});
+		mounted = true
+
+	})
 </script>
+<style>
+	.enable {
+		min-width: 230px;
+	}
+	
+</style>
 
+{#if mounted}
 <Box title={$_("config.titles.ocpp")} icon="custom:arduinoocpp" has_help={true} back={true}>
 	<div slot="help">
 		<OcppHelp />
 	</div>
-	<div class="pb-2">
-		<div class="mb-2">
-			<Switch name="ocpp_switch" label={$_("config.ocpp.enable")} onChange={toggleOCPP} bind:checked={$config_store.ocpp_enabled}/>
-		</div>
-		{#key $status_store.ocpp_connected}
-		<div class:is-hidden={!$config_store.ocpp_enabled} class="has-text-weight-bold {$status_store.ocpp_connected?"has-text-primary":"has-text-danger"}">
-			{$_("config.ocpp.connected")}: {$status_store.ocpp_connected?$_("yes"):$_("no")}
-		</div>
-		{/key}
-		<InputForm title="{$_("config.ocpp.server")}*:" bind:value={$config_store.ocpp_server} placeholder="wss://domain/steve/websocket/CentralSystemService" />
-		<div class="mr-2">
-			<InputForm title="{$_("config.ocpp.chargeBoxId")}*:" bind:value={$config_store.ocpp_chargeBoxId} is_inline />
-		</div>
-		<div class="mb-4">
-			<InputForm title="{$_("config.ocpp.authkey")}*:" type="password" bind:value={$config_store.ocpp_authkey} placeholder={$_("config.ocpp.authkey-desc")}/>
-		</div>
-		<div class="is-flex is-flex-direction-column is-align-items-center is-justify-content-center">
-			<Borders>
-				<div class="m-4">
-					<div class="has-text-weight-bold has-text-info mb-2">{$_("config.ocpp.auth")}</div>
-					<div class="is-flex is-flex-direction-column is-align-items-start mb-4">
-							<Switch name="ocpp_auto" label={$_("config.ocpp.auth-auto")} bind:checked={$config_store.ocpp_auth_auto}/>
-						<div class:is-hidden={!$config_store.ocpp_auth_auto}>
-							<InputForm title="{$_("config.ocpp.idtag")}*:" bind:value={$config_store.ocpp_idtag} placeholder="F4D1A7694ECD21" is_inline />
-						</div>
-							<Switch name="ocpp_auth_offline" label={$_("config.ocpp.auth-offline")} bind:checked={$config_store.ocpp_auth_offline}/>
+	<div class="columns is-centered">
+		<div class="column is-three-quarters is-full-mobile">
+			
+			<div class="mb-2 enable is-flex is-align-items-center is-justify-content-center">
+				<Borders classes={$config_store.ohm_enabled?"has-background-primary-light":"has-background-light"}>
+					<Switch 
+						name="ocppswitch" 
+						label={$_("enable")} 
+						onChange={toggleOCPP} 
+						bind:this={formdata.ocpp_enabled.input} 
+						bind:checked={formdata.ocpp_enabled.val} 
+						bind:status={formdata.ocpp_enabled.status} 
+						disabled={formdata.ocpp_enabled.status == "loading"}
+					/>
+					{#key $status_store.ocpp_connected}
+					<div class:is-hidden={!$config_store.ocpp_enabled} class="has-text-weight-bold {$status_store.ocpp_connected?"has-text-primary":"has-text-danger"}">
+						{$_("config.ocpp.connected")}: {$status_store.ocpp_connected?$_("yes"):$_("no")}
 					</div>
-					<div class="has-text-weight-bold has-text-info my-2">{$_("config.ocpp.controls")}</div>
-					<div class="is-flex is-flex-direction-column is-align-items-start">
-						<Switch name="ocpp_suspend" label={$_("config.ocpp.suspend-evse")} bind:checked={$config_store.ocpp_suspend_evse}/>
-						<Switch name="ocpp_energize_plug" label={$_("config.ocpp.energize-plug")} bind:checked={$config_store.ocpp_energize_plug}/>
+					{/key}
+				</Borders>
+			</div>
+			<div class="mb-2 is-flex is-align-items-center is-justify-content-center">
+				<Borders grow>
+					<div class="mr-2">
+						<InputForm
+							title="{$_("config.ocpp.server")}*" 
+							type="text"
+							bind:this={formdata.ocpp_server.input} 
+							bind:value={formdata.ocpp_server.val} 
+							bind:status={formdata.ocpp_server.status} 
+							disabled={formdata.ocpp_server.status == "loading"} 
+							placeholder="wss://domain/steve/websocket/CentralSystemService" 
+							onChange={()=>setProperty("ocpp_server")}
+						/>
 					</div>
-				</div>
-			</Borders>
+
+					
+					<div class="mr-2">
+						<InputForm
+						title="{$_("config.ocpp.chargeBoxId")}*" 
+						type="text"
+						bind:this={formdata.ocpp_chargeBoxId.input} 
+						bind:value={formdata.ocpp_chargeBoxId.val} 
+						bind:status={formdata.ocpp_chargeBoxId.status} 
+						disabled={formdata.ocpp_chargeBoxId.status == "loading"} 
+						onChange={()=>setProperty("ocpp_chargeBoxId")}
+					/>
 		
+					</div>
+					<div class="mr-2">
+						<InputForm
+							title="{$_("config.ocpp.authkey")}*" 
+							type="password"
+							bind:this={formdata.ocpp_authkey.input} 
+							bind:value={formdata.ocpp_authkey.val} 
+							bind:status={formdata.ocpp_authkey.status} 
+							disabled={formdata.ocpp_authkey.status == "loading"} 
+							placeholder={$_("config.ocpp.authkey-desc")}
+							onChange={()=>setProperty("ocpp_authkey")}
+						/>			
+					</div>
+				</Borders>
+			</div>
+			<div class="is-flex is-flex-direction-column is-align-items-center is-justify-content-center">
+				<Borders grow>
+					<div class="m-4">
+						<div class="has-text-weight-bold has-text-info mb-2">{$_("config.ocpp.auth")}</div>
+						<div class="is-flex is-flex-direction-column is-align-items-start mb-4">
+							<Switch 
+								name="ocpp_auto" 
+								label={$_("config.ocpp.auth-auto")} 
+								onChange={()=>setProperty("ocpp_auth_auto")} 
+								bind:this={formdata.ocpp_auth_auto.input} 
+								bind:checked={formdata.ocpp_auth_auto.val} 
+								bind:status={formdata.ocpp_auth_auto.status} 
+								disabled={formdata.ocpp_auth_auto.status == "loading"}
+							/>
+							<div class:is-hidden={!$config_store.ocpp_auth_auto}>
+								<InputForm
+									title="{$_("config.ocpp.idtag")}*" 
+									type="text" is_inline
+									bind:this={formdata.ocpp_idtag.input} 
+									bind:value={formdata.ocpp_idtag.val} 
+									bind:status={formdata.ocpp_idtag.status} 
+									disabled={formdata.ocpp_idtag.status == "loading"} 
+									placeholder="F4D1A7694ECD21"
+									onChange={()=>setProperty("ocpp_idtag")}
+								/>	
+							</div>
+							<Switch 
+								name="ocpp_auth_offline" 
+								label={$_("config.ocpp.auth-offline")} 
+								onChange={()=>setProperty("ocpp_auth_offline")} 
+								bind:this={formdata.ocpp_auth_offline.input} 
+								bind:checked={formdata.ocpp_auth_offline.val} 
+								bind:status={formdata.ocpp_auth_offline.status} 
+								disabled={formdata.ocpp_auth_offline.status == "loading"}
+							/>	
+						</div>
+						<div class="has-text-weight-bold has-text-info my-2">{$_("config.ocpp.controls")}</div>
+						<div class="is-flex is-flex-direction-column is-align-items-start">
+							<Switch 
+								name="ocpp_suspend" 
+								label={$_("config.ocpp.suspend-evse")} 
+								onChange={()=>setProperty("ocpp_suspend_evse")} 
+								bind:this={formdata.ocpp_suspend_evse.input} 
+								bind:checked={formdata.ocpp_suspend_evse.val} 
+								bind:status={formdata.ocpp_suspend_evse.status} 
+								disabled={formdata.ocpp_suspend_evse.status == "loading"}
+							/>
+							<Switch 
+							name="ocpp_energize_plug" 
+							label={$_("config.ocpp.energize-plug")} 
+							onChange={()=>setProperty("ocpp_energize_plug")} 
+							bind:this={formdata.ocpp_energize_plug.input} 
+							bind:checked={formdata.ocpp_energize_plug.val} 
+							bind:status={formdata.ocpp_energize_plug.status} 
+							disabled={formdata.ocpp_energize_plug.status == "loading"}
+						/>
+						</div>
+					</div>
+				</Borders>
+			</div>
 		</div>
-		<div class="block mt-2 ">
-			<Button name={$_("save")} color="is-info" state={stg_submit_state} butn_submit={stg_submit} />
-		</div>
-		<AlertBox title={$_("error")}  body={alert_body} bind:visible={alert_visible} />
 	</div>
 </Box>
+{/if}
