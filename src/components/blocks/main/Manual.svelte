9296cdebb5469022d900a7e45f2e94e42588e34b
KipK <hotlobster75@gmail.com>
2023-01-29 18:34:08 +0100

    better charge current slider response

--- src/components/blocks/main/Manual.svelte
+++ src/components/blocks/main/Manual.svelte
@@ -31,7 +31,7 @@
 	let charge_lmt_status = ""
 
 	async function setChgCurrent(val) {
-		if ($uistates_store.charge_current == val)
+		if ($claims_target_store.properties.charge_current == val)
 			return
 		$uistates_store.charge_current = val
 		if (val == $config_store.max_current_soft) {
@@ -282,8 +282,15 @@ $: set_uistates_divertmode($status_store.divertmode)
 		</div>
 	
 		<div class="container mb-2">
-			<Slider icon="fa6-solid:gauge-high" tooltip={$_("charge-rate-ttip")} unit="A" min=6 max={$config_store.max_current_soft} step={1} label={$_("charge-rate-label")} disabled={EvseClients[clientid2name($claims_target_store.claims?.charge_current)]?.priority > EvseClients.manual.priority || waiting} 
-			value={$uistates_store.charge_current} onchange={(value) => setChgCurrent(value)} />
+			<Slider 
+				icon="fa6-solid:gauge-high" 
+				tooltip={$_("charge-rate-ttip")} 
+				unit="A" min=6 max={$config_store.max_current_soft} step={1} 
+				label={$_("charge-rate-label")} 
+				disabled={EvseClients[clientid2name($claims_target_store.claims?.charge_current)]?.priority > EvseClients.manual.priority || waiting} 
+				bind:value={$uistates_store.charge_current}
+				onchange={(value) => setChgCurrent(value)} 
+			/>
 			{#if $claims_target_store?.claims?.charge_current && $claims_target_store.claims.charge_current != EvseClients.timer.id}
 			<div class="is-flex is-justify-content-center is-align-content">
 				<RemovableTag bind:this={setamp_tag} client={$claims_target_store.claims.charge_current} action={()=>removeProp("charge_current",setamp_tag)} />
