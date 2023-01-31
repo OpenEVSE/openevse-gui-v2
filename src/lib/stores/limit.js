bcb91e154ec05aeae6c822fe366a9165f9f7ad91
KipK <hotlobster75@gmail.com>
2023-01-31 21:00:18 +0100

    add limit_store

---
+++ src/lib/stores/limit.js
@@ -0,0 +1,48 @@
+import { writable }         from 'svelte/store'
+import {httpAPI}            from '../utils.js'
+
+let model = { type: "none", value: 0}
+
+function createLimitStore() {
+    const P  = writable(model)
+    const { subscribe, set, update } = P
+
+	async function download() {
+            let res = await httpAPI("GET", "/limit")
+            if (res && res.hasOwnProperty("type")) {
+                P.update(() => res)
+                return true
+            }
+            else if (res && res.hasOwnProperty("msg") && res.msg == "no limit") {
+                // reset limit to default
+                P.update(()=>model)
+                return true
+            }
+            else return false
+	}
+
+    async function upload(data) {
+        let res = await httpAPI("POST", "/limit",JSON.stringify(data))
+        if (res.msg == "done")
+            return true
+        else return false
+    }
+
+    async function remove(id) {
+        let res = await httpAPI("DELETE", "/limit")
+        if (res.msg == "done")
+            return true
+        else return false
+    }
+
+    return {
+        subscribe,
+        set,
+        update,
+        download,
+        remove: (id) => remove(id),
+        upload: (data) => upload(data)
+    }
+}
+
+export const limit_store = createLimitStore()
