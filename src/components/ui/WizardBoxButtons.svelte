e786e114bac9cb6b03952351c1e4c3df3fb42e78
Jeremy Poulter <jpoulter@webgains.com>
2023-09-12 23:12:49 +0100

    Fix for Quit Qizard button not working
    Fixes #690
    
    serialQueue expects a function, so wrapped call to saveParam as with other calls.

--- src/components/ui/WizardBoxButtons.svelte
+++ src/components/ui/WizardBoxButtons.svelte
@@ -23,21 +23,21 @@
 	async function quit() {
 		$uistates_store.wizard_step = 0
 		if (!$config_store.wizard_passed)
-			await serialQueue.add(config_store.saveParam("wizard_passed", true))
+			await serialQueue.add(() => config_store.saveParam("wizard_passed", true))
 		if ($status_store.ipaddress == "192.168.4.1") {
 			$uistates_store.alertbox.title = $_("notification")
 			$uistates_store.alertbox.component = AlertBody
 			$uistates_store.alertbox.visible = true
 			$uistates_store.alertbox.button = true
 			$uistates_store.alertbox.closable = false
-			$uistates_store.alertbox.action = () => { 
+			$uistates_store.alertbox.action = () => {
 				uistates_store.resetAlertBox()
 				setTimeout(() => {
 					reload2ip()
 				}, 6000);
 			}
 		}
-		else 
+		else
 			window.location.href = "http://" + $status_store.ipaddress
 	}
 
@@ -46,14 +46,14 @@
 <div class="is-flex is-justify-content-center my-3">
 	{#if step > 0}
 	<button class="button is-white mr-4" on:click={goPrev}>
-		<iconify-icon class="is-size-5" icon="material-symbols:navigate-before"></iconify-icon> 
+		<iconify-icon class="is-size-5" icon="material-symbols:navigate-before"></iconify-icon>
 		Previous &nbsp;
 	</button>
 	{/if}
 	{#if step < 4}
 	<button class="button is-white ml-4" on:click={goNext}>
 		&nbsp; Next
-		<iconify-icon class="is-size-5" icon="material-symbols:navigate-next"></iconify-icon> 
+		<iconify-icon class="is-size-5" icon="material-symbols:navigate-next"></iconify-icon>
 	</button>
 	{/if}
 	{#if step == 4}
