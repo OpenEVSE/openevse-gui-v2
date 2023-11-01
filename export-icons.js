d714ad266d1c93f9079dc061521d2809db8f2984
Jeremy Poulter <jpoulter@webgains.com>
2023-11-01 21:37:47 +0000

    Fixed script to export icons for the local TFT UI

--- export-icons.js
+++ export-icons.js
@@ -1,29 +1,96 @@
 import './src/lib/icons/icons.js';
 
 import { getIcon, listIcons } from 'iconify-icon';
-import pkg from 'convert-svg-to-png';
-const { convert } = pkg;
-import 'fs';
-import { IconSet } from '@iconify/tools';
 
+import { promises as fs } from 'fs';
+import svg2img from 'svg2img';
 
-const icons = listIcons();
-console.log(icons);
+import {
+    IconSet,
+    importDirectory,
+    cleanupSVG,
+    runSVGO,
+    parseColors,
+    isEmptyColor,
+} from '@iconify/tools';
 
-let icon = getIcon('fa6-solid:bolt');
-console.log(icon);
-
+// Load icons we want to export
 const iconSet = new IconSet({
-    prexix: 'hid',
+    prefix: 'hid',
     icons: {
-        'bolt': {
-            body: getIcon('fa6-solid:bolt').body
-        }
+        'rocket': getIcon('majesticons:rocket-3-start-line'),
+        'car-off': getIcon('mdi:car-off'),
+        'car': getIcon('mdi:car'),
+        'bolt': getIcon('fa6-solid:bolt'),
+        'error': getIcon('fluent:shield-error-24-filled'),
+        'ban': getIcon('fa6-solid:ban'),
     }
 });
 
-let svg = iconSet.getSVG('bolt');
-let png = await convert(svg);
 
-// save the PNG
-fs.writeFileSync('bolt.png', png);
+(async () => {
+    await iconSet.forEach(async (name, type) => {
+        if (type !== 'icon') {
+            return;
+        }
+
+        const svg = iconSet.toSVG(name);
+        if (!svg) {
+            // Invalid icon
+            iconSet.remove(name);
+            return;
+        }
+
+        // Clean up and optimise icons
+        try {
+            // Cleanup icon code
+            await cleanupSVG(svg);
+
+            // Assume icon is monotone: replace color with currentColor, add if missing
+            // If icon is not monotone, remove this code
+            //await parseColors(svg, {
+            //    defaultColor: 'currentColor',
+            //    callback: (attr, colorStr, color) => {
+            //        return !color || isEmptyColor(color) ? colorStr : 'currentColor';
+            //    },
+            //});
+
+            // Optimise
+            await runSVGO(svg);
+        } catch (err) {
+            // Invalid icon
+            console.error(`Error parsing ${name}:`, err);
+            iconSet.remove(name);
+            return;
+        }
+
+        // Update icon
+        iconSet.fromSVG(name, svg);
+    });
+
+    // Export all icons
+    await iconSet.forEach(async (name) => {
+        const svg = iconSet.toString(name);
+        if (!svg) {
+            return;
+        }
+
+        // Save SVG to file
+        // await fs.writeFile(`output/${name}.svg`, svg, 'utf8');
+        // console.log(`Saved output/${name}.svg (${svg.length} bytes)`);
+
+        svg2img(svg, {
+                resvg: {
+                    fitTo: {
+                        mode: 'width', // or height
+                        value: 24,
+                    },
+                }
+            },
+            function (error, buffer) {
+                // returns a Buffer
+                fs.writeFile(`output/${name}.png`, buffer);
+                console.log(`Saved output/${name}.png (${buffer.length} bytes)`);
+            });
+    });
+})();
