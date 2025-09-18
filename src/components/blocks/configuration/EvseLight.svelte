c20bca40a9c523512adf5b35a1d315d72d8883ae
Jeremy Poulter <jpoulter@webgains.com>
2025-09-18 15:47:01 +0100

    Adding UI for setting the one time write max hardware setting in setup wizard

--- src/components/blocks/configuration/EvseLight.svelte
+++ src/components/blocks/configuration/EvseLight.svelte
@@ -8,26 +8,42 @@
 	import InputForm 			 from "./../../ui/InputForm.svelte";
 	import Box 					 from "./../../ui/Box.svelte";
 	import { submitFormData } 	 from "./../../../lib/utils.js"
+	import Switch from "./../../ui/Switch.svelte";
 
 	let mounted = false
 	const states_items = [{name: $_("disabled"), value: false}, {name: $_("active"), value: true}]
 	const phase_items = [{name: $_("no"), value: false}, {name: $_("yes"), value: true}]
 	
 	let formdata = {
+			max_current: 			{val: false,  status: "", input: undefined, req: false},
 			max_current_soft: 		{val: false,  status: "", input: undefined, req: false},
+			max_current_hard: 		{val: false,  status: "", input: undefined, req: false},
 			default_state:			{val: false,  status: "", input: undefined, req: false},
 			is_threephase:			{val: false,  status: "", input: undefined, req: false},
 			scheduler_start_window:	{val: null,   status: "", input: undefined, req: false}
-		}	
+	}
+
+	let set_hardware_max = false;
+	let hardware_max_disabled = false;
 
 	let updateFormData = () => {
+		formdata.max_current.val = $config_store.max_current_soft
 		formdata.max_current_soft.val = $config_store.max_current_soft
+		formdata.max_current_hard.val = $config_store.max_current_hard
 		formdata.default_state.val = $config_store.default_state
 		formdata.is_threephase.val = $config_store.is_threephase
 		formdata.scheduler_start_window.val = $config_store.scheduler_start_window
 	}
 
 	let setProperty = async (prop) => {
+		if("max_current_hard" == prop) {
+			formdata.max_current_hard.val = formdata.max_current.val;
+			set_hardware_max = false;
+			hardware_max_disabled = true;
+		}
+		if ("max_current_soft" == prop) {
+			formdata.max_current_soft.val = formdata.max_current.val
+		}
 		await submitFormData({prop: prop, form: formdata})
 	}
 	
@@ -45,12 +61,25 @@
 				<Borders grow={true}>
 					<div class="has-text-weight-bold is-size-6">{$_("config.evse.maxcur")}</div>
 					<SliderForm icon="fa6-solid:gauge-high" 
-						bind:value={formdata.max_current_soft.val} 
+						bind:value={formdata.max_current.val} 
 						unit={$_("units.A")} 
 						min={$config_store.min_current_hard?$config_store.min_current_hard:6} 
 						max={$config_store.max_current_hard?$config_store.max_current_hard:32} 
-						onchange={()=>setProperty("max_current_soft")} 
+						onchange={()=>{ if(false === set_hardware_max) { setProperty("max_current_soft")}}} 
 					/>
+					<div class="mt-2">
+						<Switch name="set_hardware_max" bind:checked={set_hardware_max} label="{$_("config.evse.set_hardware_max")}" disabled={hardware_max_disabled} />
+					</div>
+					{#if set_hardware_max}
+						<div class="notification is-warning mt-2">
+						{@html $_("config.evse.set_hardware_max_warning")}
+						<div class="mt-2 has-text-centered">
+							<button class="button" on:click={() => setProperty("max_current_hard")}>
+								{$_("config.evse.confirm_set_hardware_max")}
+							</button>
+						</div>
+						</div>
+					{/if}
 				</Borders>
 			</div>
 			{#if $config_store.default_state !== 'undefined'}
